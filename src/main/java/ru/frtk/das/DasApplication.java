cbed5694519cd96e92b890f1e98f7e706aa60385
Morgen Matvey <melges.morgen@gmail.com>
2018-03-03 03:12:16 +0300

    Init project with spring-boot.

---
+++ src/main/java/ru/frtk/das/DasApplication.java
@@ -0,0 +1,12 @@
+package ru.frtk.das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasApplication.class, args);
+	}
+}
